edc4f16748e236988ef02ed6c3183f011ce2adeb
UNO <a.baena@andaluciajunta.es>
2017-11-20 12:42:45 +0100

    prueba

---
+++ app/src/main/java/com/example/uno/a201117/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.uno.a201117;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    static EditText uri;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        inicializarUI();
+    }
+
+    public void inicializarUI(){
+        uri = findViewById(R.id.txtURL);
+        //probando
+    }
+
+
+    public void ejecutaURL(View v){
+        Intent intent = new Intent(Intent.ACTION_WEB_SEARCH);
+        intent.setData(Uri.parse(uri.getText().toString()));
+        startActivity(intent);
+    }
+
+
+}
