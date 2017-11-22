f896371fc5bf12c709d891ba818aa5e017333cad
UNO <a.baena@andaluciajunta.es>
2017-11-22 09:47:08 +0100

    probando commit

--- app/src/main/java/com/example/uno/a201117/MainActivity.java
+++ app/src/main/java/com/example/uno/a201117/MainActivity.java
@@ -15,8 +15,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         inicializarUI();
-
-
     }
 
     public void inicializarUI(){
